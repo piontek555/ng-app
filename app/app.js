6feb78af907fbc42867b623d010619eb262d2e83
Cezary Prokopowicz <cezary.prokopowicz@pagepro.pl>
2017-04-28 15:35:38 +0200

    Leatest verision

--- app/app.js
+++ app/app.js
@@ -1,77 +1,16 @@
 (function() {
-  'use strict';
+  "use strict";
 
-  angular.module('ngApp', [])
-    .controller('mainController', mainController);
-
-  function mainController($scope) {
-    $scope.gameList = [
-    	{
-			title: 'Tomb Raider',
-			platform: 'ps3',
-			completions: ['2014-2015', '2016'],
-		  },
-      {
-      title: 'Metal Gear Solid: 4',
-      platform: 'ps3',
-      completions: ['2013', '2016-xxxx'],
-      },
-	 ];
-   $scope.gamePlatformsOptions = ['please choose a platform', 'psx', 'ps2', 'Add new platform'];
-    // $scope.formSubmit = formSubmit;
-    $scope.addCompletion = addCompletion;
-    $scope.removeEl = removeEl;
-    $scope.addNewPlatform = addNewPlatform;
-    $scope.gameTitle = '';
-    $scope.gamePlatform = 'psx';
-    $scope.completionsList = [];
-
-    $scope.validMessage = {
-      validRequired: 'Please fill required field'
-    };
-
-    $scope.formSubmit = function () {
-      if(!$scope.gameForm.$invalid) {
-        alert('Great! Game has been added!');
-        $scope.reset(gameForm);
-      } else {
-        return
-      }
-
-      $scope.gameList.push({
-        title: $scope.gameTitle,
-        platform: $scope.gamePlatform,
-        completions: $scope.completionsList
-      });
-
-      console.log($scope.gameList);
-      $scope.gameTitle = '';
-    	$scope.gamePlatform = '';
-    	$scope.gameCompletion = ''
-    	$scope.completionsList = [];
-    }
-
-    //rest form
-    $scope.reset = function(thisForm) {
-      console.log('forms');
-      console.log(thisForm);
-      console.log(gameForm);
-        $scope.gameForm.$setPristine();
-        $scope.gameForm.$setUntouched();
-    }
-
-    function addCompletion() {
-    	$scope.completionsList.push($scope.gameCompletion);
-    	$scope.gameCompletion = '';
-    	console.log($scope.completionsList);
-    }
-
-    function removeEl(arrayName, idx) {
-      arrayName.splice(idx, 1);
-    }
-    function addNewPlatform () {
-      $scope.gamePlatformsOptions.push($scope.newPlatform);
-      $scope.gamePlatform = $scope.newPlatform;
-    }
-  }
+  angular.module('ngApp', ['ngRoute'])
+    .config(function($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'app/gamelist.html',
+          controller: 'mainController' //zmienic nazwe na wilkie litery
+        })
+        .when('/gamedetails/:id', {
+          templateUrl: 'app/gamedetails.html',
+          controller: 'gameDetailsController'
+        })
+    });
 })();
